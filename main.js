9db09e9191da94818047a79add71c44bb07a507c
saif0110 <khanmohdtauheed4@gmail.com>
2025-01-01 10:13:51 +0530

    file changed

--- main.js
+++ main.js
@@ -3,10 +3,13 @@ const port = 3000;
 
 const server = http.createServer((req, res) => {
   // TODO: Send the ""Hello, world!"" response
+  res.write('Hello, world!');
+  res.end();
 });
 
 server.listen(port, () => {
   // TODO: Log a message to the console when the server starts listening on the port
+  console.log('Server listening on port 3000');
 });
 
 // DO NOT EDIT BELOW THIS LINE
